09f09411e4e49705618fdd3392a3e9a09b46079a
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-11-12 14:57:50 +1100

    Dice Test

--- HeroGameAdvancedTests/Dice.cs
+++ HeroGameAdvancedTests/Dice.cs
@@ -6,10 +6,28 @@ namespace HeroGameAdvancedTests
 {
     public class DiceTests
     {
-        [Fact]
-        public void Test1()
-        {
+        Dice Dice = new Dice(0, 6);
 
+        [Theory]
+        [InlineData(0, 6)]
+        [InlineData(1, 0)]
+        [InlineData(-7, 3)]
+        [InlineData(5, 10)]
+        public void TestRoll(int min, int max)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int testroll = this.Dice.Roll();
+                Assert.Equal(testroll < this.Dice.Min, false);
+                Assert.Equal(testroll > this.Dice.Max, false);
+            }  
+
+            Assert.Equal(min <= max, true);
+            Assert.Equal(max >= min, true);
+            Assert.Equal(min >= this.Dice.Min, true);
+            Assert.Equal(max <= this.Dice.Max, true);
+
+            
         }
     }
 }
