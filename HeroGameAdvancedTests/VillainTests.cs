ee21bfd43176682aca28f312dc066b0140d26e48
maxbarrah <79820153+maxbarrah@users.noreply.github.com>
2021-11-12 15:22:14 +1100

    Villain Tests completes

--- HeroGameAdvancedTests/VillainTests.cs
+++ HeroGameAdvancedTests/VillainTests.cs
@@ -6,13 +6,19 @@ namespace HeroGameAdvancedTests
 {
     public class VillainTests
     {
-        [Fact]
-        public void Test1()
+                
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(3, 3)]
+        [InlineData(2, 15)]
+        
+        public void Test(int expected, int input)
         {
-
+            Villain testvillain = new Villain(1, "Test", 20);
+            testvillain.Damage(input);
+            Assert.Equal(expected, testvillain.Hitpoints);
         }
     }
 }
-
 // applies amount of damage to Hitpoints.
 // if damage is greater that Hitpoints, Hitpoints should be set to 0 (zero)
